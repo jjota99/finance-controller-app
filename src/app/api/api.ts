19b625c6dc1a1c8f351c0ca7c060d2272ff089e5
Jota <jguilherme1999@gmail.com>
2024-03-20 17:30:29 -0300

    add axios

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -1,6 +1,6 @@
-import { ApisauceInstance, create } from 'apisauce'
+import axios, { AxiosInstance } from 'axios'
 
-export const api: ApisauceInstance = create({
+export const api: AxiosInstance = axios.create({
     baseURL: 'http://localhost:8080/api',
     headers: { Accept: 'application/vnd.github.v3+json' },
     timeout: 30,
