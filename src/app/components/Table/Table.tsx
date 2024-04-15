df2b59e9549857c437e89bd05758ca86ce4e8996
Jota <jguilherme1999@gmail.com>
2024-04-15 15:04:45 -0300

    finish dropdown and layout improvments

--- src/app/components/Table/Table.tsx
+++ src/app/components/Table/Table.tsx
@@ -1,6 +1,7 @@
 import TableHeader from '@/app/components/Table/components/TableHeader/TableHeader'
 import TableBody from '@/app/components/Table/components/TableBody/TableBody'
 import { TTransactions } from '@/app/types/mainDashboard'
+import TableFooter from '@/app/components/Table/components/TableFooter/TableFooter'
 
 type Props = {
     columns: {
@@ -19,6 +20,7 @@ export default function Table({ columns, rows }: Props) {
         <table className="w-full">
             <TableHeader columns={columns} />
             <TableBody rows={rows} />
+            <TableFooter pages={[1, 2, 3]} />
         </table>
     )
 }
